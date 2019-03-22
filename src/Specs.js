7af789ec98110605a66a092debecf453b63b958f
David Bolin <david@bolin.at>
2019-03-22 13:26:54 -0700

    render for Specs

--- src/Specs.js
+++ src/Specs.js
@@ -8,12 +8,10 @@ export default class Specs extends React.Component {
     render(){
       return  (
         <section className="main__form">
-       <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-       <Select />
-       <Select />
-       <Select />
-       <Select />
-     </section>
+         <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+         {Object.keys(this.props.FEATURES2).map(name =>
+           <Select feature={name} details={this.props.FEATURES2[name]} />)}
+        </section>
         )
     }
    
